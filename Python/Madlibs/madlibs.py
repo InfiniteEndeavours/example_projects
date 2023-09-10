126adefe65d3cbf740a06d76c7864e836afd62d4
Ethan Pitt <ShadowOfNyxx@proton.me>
2023-09-10 21:23:54 +0100

    Create madlibs game

---
+++ Python/Madlibs/madlibs.py
@@ -0,0 +1,20 @@
+print("Please enter your words below: \n")
+verb1 = input("Verb: ")
+adjective1 = input("Adjective: ")
+verb2 = input("Verb: ")
+body_part1 = input("Body Part: ")
+adverb1 = input("Adverb: ")
+body_part2 = input("Body Part: ")
+noun1 = input("Noun: ")
+verb3 = input("Verb: ")
+animals = input("Animals: ")
+noun2 = input("Noun: ")
+verb4 = input("Verb: ")
+adjective2 = input("Adjective: ")
+color = input("Color: ")
+
+story = f"""
+Most doctors agree that bicycle of {verb1} is a/an {adjective1} form of exercise. {verb2} a bicycle enables you to develop your {body_part1} muscles as well as {adverb1} increase the rate of a {body_part2} beat. More {noun1} around the world {verb3}  bicycles than drive {animals}. No matter what kind of {noun2} you {verb4}, always be sure to wear a/an {adjective2} helmet. Make sure to have {color} reflectors too!
+"""
+
+print(story)
